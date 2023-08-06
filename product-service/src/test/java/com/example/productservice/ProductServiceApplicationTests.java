322638c543ee287cffe5b9cb78f277602c5807b4
Subhanshu-2411 <subhashu20135@iiitd.ac.in>
2023-08-07 02:49:56 +0530

    Added Testcontainers library and Added Configurations

--- product-service/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
@@ -2,9 +2,24 @@ package com.example.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:5.0.7");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.data.monngodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Test
     void contextLoads() {
