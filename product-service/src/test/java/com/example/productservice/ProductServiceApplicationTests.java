dd86a630f0aa845e4d0871d5ea64dc5298f60da4
Subhanshu-2411 <subhashu20135@iiitd.ac.in>
2023-08-08 07:32:33 +0530

    Created Post and Get Application Tests

--- product-service/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
@@ -1,20 +1,39 @@
 package com.example.productservice;
 
+import com.example.productservice.dto.ProductRequest;
+import com.example.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
     @Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:5.0.7");
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private ObjectMapper objectMapper;
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
@@ -22,7 +41,28 @@ class ProductServiceApplicationTests {
     }
 
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/product/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(getProductRequest())))
+                .andExpect(status().isCreated());
+        Assertions.assertEquals(1, productRepository.findAll().size());
+    }
+
+    @Test
+    void shouldGetProduct() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/product/get")
+        ).andExpect(status().isOk());
+        Assertions.assertEquals(1, productRepository.findAll().size());
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .name("IPhone 13")
+                .description("IPhone 13, 8GB RAM & 128GB")
+                .price(1200.0)
+                .build();
     }
 
 }
